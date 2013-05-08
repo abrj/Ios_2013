2369081081fbc04b8e14049d169e47a71ef3d59d
Andersbrorup <abrj@itu.dk>
2013-05-08 14:50:18 +0200

    started to make the PhotoListController

---
+++ ProjectApp/PhotoListTVC.m
@@ -0,0 +1,134 @@
+//
+//  PhotoListTVC.m
+//  ProjectApp
+//
+//  Created by Anders Jorgensen on 08/05/13.
+//  Copyright (c) 2013 Abrj & Kdan. All rights reserved.
+//
+
+#import "PhotoListTVC.h"
+#import "FlickrFetcher.h"
+
+@interface PhotoListTVC ()
+
+@property (nonatomic, strong) NSArray *photos;
+@end
+
+@implementation PhotoListTVC
+
+
+- (void)setPhotos:(NSArray *)photos
+{
+    _photos = photos;
+    [self.tableView reloadData];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.photos = [FlickrFetcher stanfordPhotos];
+    NSLog(@"photos from stanford %@", self.photos);
+
+    
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.photos count];
+}
+
+// a helper method that looks in the Model for the photo dictionary at the given row
+//  and gets the title out of it
+
+- (NSString *)titleForRow:(NSUInteger)row
+{
+    NSDictionary *dic = [self.photos objectAtIndex:row];
+    NSLog(@"dic %@" , dic);
+    return [[dic objectForKey:@"title"] description];
+    //return [self.photos[row][FLICKR_PHOTO_TITLE] description]; // description because could be NSNull
+}
+
+// a helper method that looks in the Model for the photo dictionary at the given row
+//  and gets the owner of the photo out of it
+
+- (NSString *)subtitleForRow:(NSUInteger)row
+{
+    return [self.photos[row][FLICKR_PHOTO_OWNER] description]; // description because could be NSNull
+}
+
+// loads up a table view cell with the title and owner of the photo at the given row in the Model
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"FlickrPhoto";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    cell.textLabel.text = [self titleForRow:indexPath.row];
+    cell.detailTextLabel.text = [self subtitleForRow:indexPath.row];
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     [detailViewController release];
+     */
+}
+
+@end
